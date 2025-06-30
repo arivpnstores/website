d4705f8050d1112c9351581a58405653c6de7382
ARI VPN STORES <kcepu877@gmail.com>
2025-06-30 22:44:38 +0700

    oke

--- riquest_panel/script.js
+++ riquest_panel/script.js
@@ -2,11 +2,24 @@ function sendWhatsAppRequest() {
     var form = document.getElementById('requestForm');
     var formData = new FormData(form);
 
-    // Build WhatsApp message with form data
+    // Build WhatsApp message with form data and pricing
     var message = "BELI%20PANEL%20DO%202%20BULAN%0A";
+    var totalPrice = 0;
+    
     formData.forEach(function(value, key) {
-        message += key + "%3A%20" + value + "%0A";
+        if (key === "DROP") {
+            if (value === "3") {
+                message += "DROPLET%203%20-%20$270%20(2%20bulan)%0A";
+                totalPrice += 270;
+            } else if (value === "10") {
+                message += "DROPLET%2010%20-%20$900%20(2%20bulan)%0A";
+                totalPrice += 900;
+            }
+        }
     });
+    
+    message += "%0ATotal%3A%20$" + totalPrice + "%0A";
+    message += "PANEL%20DO%20BILLING%20PAYPAL";
 
     // Construct the WhatsApp URL with the message
     var whatsappUrl = "https://wa.me/6281327393959?text=" + message ;
--- riquest_panel1/script.js
+++ riquest_panel1/script.js
@@ -2,11 +2,24 @@ function sendWhatsAppRequest() {
     var form = document.getElementById('requestForm');
     var formData = new FormData(form);
 
-    // Build WhatsApp message with form data
+    // Build WhatsApp message with form data and pricing
     var message = "BELI%20PANEL%20DO%201%20TAHUN%0A";
+    var totalPrice = 0;
+    
     formData.forEach(function(value, key) {
-        message += key + "%3A%20" + value + "%0A";
+        if (key === "DROP") {
+            if (value === "3") {
+                message += "DROPLET%203%20-%20$1620%20(1%20Tahun)%0A";
+                totalPrice += 1620;
+            } else if (value === "10") {
+                message += "DROPLET%2010%20-%20$5400%20(1%20Tahun)%0A";
+                totalPrice += 5400;
+            }
+        }
     });
+    
+    message += "%0ATotal%3A%20$" + totalPrice + "%0A";
+    message += "PANEL%20DO%20BILLING%20PAYPAL";
 
     // Construct the WhatsApp URL with the message
     var whatsappUrl = "https://wa.me/6281327393959?text=" + message ;
