51f600df3467f13d2ef63e60c50cb73d248074d9
ARI VPN STORES <arivpnstores@gmail.com>
2026-04-20 11:48:45 +0700

    up

---
+++ os/tools.sh
@@ -0,0 +1,79 @@
+ tools.shvless
+mkdir -p /etc/trojan
+mkdir -p /root/udp
+clear
+rm -rlimit/vless
+mkdir -p /etc/kyt/limit/trojan
+mkdir -p /etc/imit/ssh
+mkdir -p /etc/kyt/limit/vmess
+mkdir -p /etc/kyt/dir -p /etc/vmess
+mkdir -p /etc/limit
+mkdir -p /etc/kyt/llly installed..."
+mkdir -p /etc/xray
+mkdir -p /etc/bot
+mk\033[33;1m${*}\\033[0m"; }
+yellow "Dependencies successfuo "           Instalasi selesai!"
+
+
+yellow() { echo -e "\cd >/dev/null 2>&1
+apt autoremove -y >/dev/null 2>&1
+
+echy --purge remove \
+  samba* apache2* bind9* sendmail* uns
+sudo apt-get autoclean -y >/dev/null 2>&1
+sudo apt-get --i /tmp/gotop.deb
+
+# Bersihkan file yang tidak diperlukanamd64.deb"
+curl -sL "$gotop_link" -o /tmp/gotop.deb
+dpkg ses/download/v$gotop_latest/gotop_v"$gotop_latest"_linux_ 1)"
+gotop_link="https://github.com/xxxserxxx/gotop/releaes | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -nrl -s https://api.github.com/repos/xxxserxxx/gotop/releasibcurl4-openssl-dev
+
+# Instalasi gotop
+gotop_latest="$(cuginx php \
+  php-fpm php-cli php-mysql p7zip-full squid lython-is-python3 python3-pip shc build-essential nodejs ngit speedtest-cli p7zip-full libjpeg-dev \
+  zlib1g-dev plex bison make \
+  libnss3-tools libevent-dev xl2tpd apt ev libcap-ng-utils libselinux1-dev libcurl4-openssl-dev f-dev libnspr4-dev pkg-config libpam0g-dev \
+  libcap-ng-dport-https gnupg2 \
+  dnsutils lsb-release chrony libnss3ear socat cron bash-completion ntpdate xz-utils apt-transl openvpn easy-rsa fail2ban tmux stunnel4 squid \
+  dropbr libxml-parser-perl \
+  neofetch screenfetch lsof openssnupg gnupg1 bc apt-transport-https build-essential dirmngoreutils rsyslog iftop htop zip unzip net-tools \
+  sed gml-parser-perl \
+  squid nmap screen curl jq bzip2 gzip ciptables-persistent netfilter-persistent figlet ruby libx_FRONTEND=noninteractive apt-get -y install \
+  iptables nf-set-selections
+
+# Instalasi paket tambahan
+sudo DEBIANtent iptables-persistent/autosave_v6 boolean true | debcooolean true | debconf-set-selections
+echo iptables-persischo iptables-persistent iptables-persistent/autosave_v4 btoremove -y
+
+# Konfigurasi iptables agar autosave aktif
+eewalld exim4
+
+# Bersihkan paket tidak terpakai
+apt-get aun yang tidak dibutuhkan
+apt-get remove --purge -y ufw fires-common --no-install-recommends
+
+# Hapus firewall bawaaall -y debconf-utils haproxy p7zip-full software-properti apt-get clean all
+
+# Instalasi utilitas penting
+apt instpgrade -y
+apt install sudo -y
+
+# Bersihkan cache apt
+sudorade sistem
+apt update -y && apt upgrade -y && apt dist-u                 Progress..."
+sleep 0.1
+
+# Update dan upg
+#!/bin/bash
+
+echo "           Tools install...!"
+echo " 033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+
+\e[1;33m'
+NC='\e[0m'
+green() { echo -e "\\033[32;1m${*}\\#!/bin/bash
+clear
+red='\e[1;31m'
+green2='\e[1;32m'
+yell='
