242a5436f90c3013dbf90e5ef08af7f1f21f6a81
ARI VPN STORES <arivpnstores@gmail.com>
2026-04-20 13:45:37 +0700

    Update installsl.sh

--- v7/slowdns/installsl.sh
+++ v7/slowdns/installsl.sh
@@ -8,7 +8,7 @@ ns_domain_cloudflare() {
 	NS_DOMAIN=ns-${SUB_DOMAIN}
 	CF_ID= Ridwanstoreaws@gmail.com
         CF_KEY=4ecfe9035f4e6e60829e519bd5ee17d66954f
-	set -euo pipefail
+	
 	IP=$(wget -qO- ipinfo.io/ip)
 	echo "Updating DNS NS for ${NS_DOMAIN}..."
 	ZONE=$(
