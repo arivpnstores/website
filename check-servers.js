38ccb1e57a5dcad9266257555522f835a248b5b6
PT RAJA SERVER PREMIUM <arivpnstores@gmail.com>
2026-07-06 19:35:57 +0700

    upss

--- check-servers.js
+++ check-servers.js
@@ -15,7 +15,7 @@ const SERVERS = [
   { name: 'EDU-3 DIRECT', host: 'eduserv3.aiosc.my.id' },
   { name: 'CLOUDFRONT REGULER', host: 'd1lnzdlpso56a0.cloudfront.net' },
   { name: 'VIP CLOUDFRONT PREMIUM', host: 'd1jrahl0hl8ghp.cloudfront.net' },
-  { name: 'BOT ALIBABA', host: 'alibaba.rajaserver.web.id' },
+  { name: 'BOT ALIBABA', host: 'bot.rajaserver.web.id' },
 ];
 
 const PORTS = [80, 443];
